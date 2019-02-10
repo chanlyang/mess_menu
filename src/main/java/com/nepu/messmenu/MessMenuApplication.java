6426e7e5d2d715c04fd016512da819a1eded0d4a
chenliang <chenliang@shuidihuzhu.com>
2019-02-10 17:47:41 +0800

    创建项目

---
+++ src/main/java/com/nepu/messmenu/MessMenuApplication.java
@@ -0,0 +1,14 @@
+package com.nepu.messmenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessMenuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessMenuApplication.class, args);
+    }
+
+}
+
